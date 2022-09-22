1d77d8ce6063c0151808efcae1ec1e485f624d72
1E10 Laptop <sample@bths.edu>
2022-09-22 11:02:29 -0400

    added more stuff

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class TipCalculator {
-}
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class TipCalculator {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat formatter = new DecimalFormat("#.##");
+
+        System.out.println("Welcome to the Tip Calculator!");
+        System.out.print("How much people are in your group?");
+        int people = scan.nextInt();
+        System.out.print("What is the tip percentage? (Choose from 0-100):");
+        int tip = scan.nextInt();
+        double priceTotal = 0;
+        double prices = 0;
+        System.out.println("Enter a cost in dollars and cents (Insert -1 to end adding prices):");
+        priceTotal = scan.nextDouble();
+        while (prices != 0) {
+            System.out.println("Enter a cost in dollars and cents (Insert -1 to end adding prices):");
+            prices = scan.nextDouble();
+            priceTotal += prices;
+            System.out.println(priceTotal);
+
+        }
+
+    }
+    }
+
