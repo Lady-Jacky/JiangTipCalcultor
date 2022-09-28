d9770381780fe5fab432d0f0d4260df0d737621e
1E10 Laptop <sample@bths.edu>
2022-09-28 10:15:10 -0400

    added some stuff

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -15,13 +15,21 @@ public class TipCalculator {
         double prices = 0;
         System.out.println("Enter a cost in dollars and cents (Insert -1 to end adding prices):");
         priceTotal = scan.nextDouble();
-        while (prices != 0) {
+        while (prices != -1) {
             System.out.println("Enter a cost in dollars and cents (Insert -1 to end adding prices):");
             prices = scan.nextDouble();
             priceTotal += prices;
-            System.out.println(priceTotal);
-
         }
+        if (prices == -1){
+            priceTotal += 1;
+        }
+        double tipper = tip/100;
+        double totalTip = priceTotal * (1-tipper);
+
+        System.out.println("----------------------------------------");
+        System.out.println("Total Bill Before Tip: " + priceTotal);
+        System.out.println("Tip Percentage: " + tip + "%");
+        System.out.println("Total tip: " + totalTip);
 
     }
     }
